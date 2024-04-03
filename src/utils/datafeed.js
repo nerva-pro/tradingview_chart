71e37037dcd0fcfc11b6bdcaaca363ac523dd1d9
Udaydeep <udaydeep.yadav@gmail.com>
2024-04-04 02:18:57 +0530

    Initial Commit

--- src/utils/datafeed.js
+++ src/utils/datafeed.js
@@ -79,7 +79,7 @@ const datafeed = {
     const { from, to, firstDataRequest } = periodParams
     // console.log('[getBars]: Method call', symbolInfo, resolution, from, to)
     fetch(
-      `${API_ENDPOINT}/history?symbol=${symbolInfo.ticker}&from=${periodParams.from}&to=${periodParams.to}&resolution=${resolution}`
+      `${API_ENDPOINT}/history?symbol=${symbolInfo.ticker}&resolution=${resolution}`//&from=${periodParams.from}&to=${periodParams.to}
     ).then((response) => {
       response
         .json()
